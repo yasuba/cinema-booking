756b6ef2474a4bc69c56c094d4104f5bc72e354f
Maya Driver <mayadrivermd@gmail.com>
2014-11-11 13:07:43 +0000

    raise error if an unavailable seat is booked

--- lib/row.rb
+++ lib/row.rb
@@ -8,19 +8,17 @@ class Row
     @seats = Array.new(50) {|i| i = Seat.new}
   end
 
-  def has_seats?
-    true
-  end
-
   def seat_count
     @seats.count
   end
 
   def book_seats(seat)
-    seat.book!
-    # @unavailable_seats = []
-    # @unavailable_seats << seat.book!
-    @available_seats = @seats.reject! { |s| !s.available? }.count
+    if seat.available?
+      seat.book!
+      @available_seats = @seats.reject! { |s| !s.available? }.count
+    else
+      raise 'That seat has already been booked'
+    end
   end
 
 end
--- spec/row_spec.rb
+++ spec/row_spec.rb
@@ -5,21 +5,20 @@ describe Row do
 
   let(:row)  {Row.new}
   let(:seat) {row.seats.first}
-  # let(:booked_seat) {double :seat, :available? => false}
 
-  it 'should have seats' do
-    expect(row).to have_seats
-  end
-
-  it 'should have 50 seats' do
+  it 'should have 50 seats when initialized' do
     expect(row.seat_count).to eq (50)
   end
 
   it 'allows available seats to be booked' do
-    # allow(seat).to receive(:book!).and_return(self)
     row.book_seats(seat)
     expect(seat).to_not be_available
     expect(row.available_seats).to eq (49)
+  end
+
+  it 'raises an error if an unavailable seat is booked' do
+    booked_seat = row.seats[1].book!
+    expect{row.book_seats(booked_seat)}.to raise_error
   end
 
 end
